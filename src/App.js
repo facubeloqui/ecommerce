d7de222d76ed1dfcf23e6be742e05a6a1c55851a
facubeloqui <facu.beloqui04@gmail.com>
2022-09-06 10:39:58 -0300

    components passed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import { Card } from './Components/Card';
+import Topbar from './Components/Topbar';
+import Navbar from './Components/Navbar';
+import Featured from './Components/Featured';
+import Categories from './Components/Categories';
+import Offer from './Components/Offer';
+import Card from './Components/Card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        <Card/>
-        </a>
-      </header>
-    </div>
+    <>
+    <Topbar/>
+    <Navbar/>
+    <Featured/>
+    <Categories/>
+    <Offer/>
+    <Card/>
+    </>
   );
 }
 
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,12 +1,16 @@
 import React from 'react'
 
-export function Card () {
+export default function Card () {
 
   return (
     <>
-    <div className="card product-item border-0 mb-4">
+    <div className="container-fluid pt-5">
+    <div className="row px-xl-5 pb-3">
+
+         <div className="col-lg-3 col-md-6 col-sm-12 pb-1">
+                <div className="card product-item border-0 mb-4">
                     <div className="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
-                        <img className="img-fluid w-100" src="img/product-6.jpg" alt=""/>
+                        <img className="img-fluid w-100" src="img/product-1.jpg" alt=""/>
                     </div>
                     <div className="card-body border-left border-right text-center p-0 pt-4 pb-3">
                         <h6 className="text-truncate mb-3">Colorful Stylish Shirt</h6>
@@ -18,7 +22,16 @@ export function Card () {
                         <a href="" className="btn btn-sm text-dark p-0"><i className="fas fa-eye text-primary mr-1"></i>View Detail</a>
                         <a href="" className="btn btn-sm text-dark p-0"><i className="fas fa-shopping-cart text-primary mr-1"></i>Add To Cart</a>
                     </div>
-    </div>
+                </div>
+            </div>
+            
+
+            
+         
+      
+      
+    </div> //Row
+    </div> //Container
     </>
   )
 }
