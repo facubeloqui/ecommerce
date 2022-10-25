ca2bb7e6cb05d13a9e1f7dae7a53c5057b3c06f5
Facundo Beloqui <facu.beloqui04@gmail.com>
2022-10-25 10:10:40 -0300

    fixed styles in cart component

--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -6,10 +6,13 @@ export default function Cart() {
     const {carrito} = useContext(CarritoContext)
   return (
     <>
-    <i className="fas fa-shopping-cart text-primary"></i>:
-    <span className="badge">{carrito.length}</span> 
+    <div className="col-lg-3 col-6 text-left">       
+      <i className="fas fa-heart text-primary"></i>
+      <span className="badge">0</span>
+      <i className="fas fa-shopping-cart text-primary"></i>
+      <span className="badge">{carrito.length}</span>
+    </div>
     <Carrito/>
-
     </>
   )
 }
